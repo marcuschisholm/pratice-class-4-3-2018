b4510a67642bafa00664ccd211ec849fcaa55df8
marcuschisholm <35776241+marcuschisholm@users.noreply.github.com>
2018-04-04 12:41:28 -0400

    Add files via upload

--- addarray.java
+++ addarray.java
@@ -1,4 +1,50 @@
+import java.util.Scanner;
 
-public class addarray {
+public class addarray{
+
+
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+Scanner scan=new Scanner (System.in);
+
+//asking for the array
+
+System.out.println("Enter 3 integers");
+
+int[]numbers=new int[3];
+
+System.out.println("Enter 3 integers");
+
+int[]numbers1=new int[3];
+
+
+
+int[]sum=new int [3];
+
+for (int z=1; z<sum.length;z++){
+
+	sum[z]=(numbers[z]+numbers1[z]);
+
+	System.out.println(sum[z]);
+
+	
+
+	
 
 }
+
+	
+
+	
+
+	}
+
+	
+
+	
+
+}
+
--- addarray1.java
+++ addarray1.java
@@ -1,4 +1,40 @@
 
 public class addarray1 {
+	
+public static void main(String[] args) {
 
-}
+
+
+
+	int[]numbers={28,22,39};
+
+	int[]numbers1={20,0,31};
+
+	 int[]sum=new int [3];
+
+	for (int z=0; z<sum.length;z++){
+
+	sum[z]=(numbers[z]+numbers1[z]);
+
+	System.out.println(sum[z]);
+
+		    
+
+		    }
+
+			
+
+			
+
+			
+
+			
+
+			
+
+		}
+
+
+
+	}
+
